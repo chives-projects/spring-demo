6ccaee3e05e9639a6fa64475c5adc6f668a11acf
achiever-or-loser <1379945771@qq.com>
2021-03-13 15:36:27 +0800

    web with thymeleaf

---
+++ web/src/main/java/com/csc/web/controller/WebController.java
@@ -0,0 +1,21 @@
+package com.csc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    /**
+     * try request http://localhost:8082/index
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = {"/index", "/", "/hello"})
+    public String index(Model model) {
+        model.addAttribute("title", "测试");
+        model.addAttribute("atext", "这个冬天不太Cool");
+        return "index";
+    }
+}
